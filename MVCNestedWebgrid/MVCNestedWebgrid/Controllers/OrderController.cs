5505505626de0cabc56102b1bb2ab46dfad49e03
kzganesan <karthikeyan.ganesan.21@gmail.com>
2015-08-25 21:59:37 -0500

    Code

---
+++ MVCNestedWebgrid/MVCNestedWebgrid/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using MVCNestedWebgrid.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNestedWebgrid.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult List()
+        {
+            List<OrderVM> allOrder = new List<OrderVM>();
+
+            // here MyDatabaseEntities is our data context
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var o = dc.OrderMasters.OrderByDescending(a => a.OrderID);
+                foreach (var i in o)
+                {
+                    var od = dc.OrderDetails.Where(a => a.OrderID.Equals(i.OrderID)).ToList();
+                    allOrder.Add(new OrderVM { order= i, orderDetails = od });
+                }
+            }
+            return View(allOrder);
+        }
+
+
+
+    }
+}
---
+++ MVCNestedWebgrid/MVCNestedWebgrid/ViewModel/OrderVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNestedWebgrid.ViewModel
+{
+    public class OrderVM
+    {
+        public OrderMaster order { get; set; }
+        public List<OrderDetail> orderDetails { get; set; }
+    }
+}
